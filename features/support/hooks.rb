461203284dd12cf06893dc03437f90ef482003eb
Jonatas Faria <jonatas1503@hotmail.com>
2020-10-29 20:01:40 -0300

    .
    .

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -35,7 +35,7 @@ at_exit do
     config.input_path = "log/report.json"
     config.report_path = "log/report"
     config.report_types = [:html]
-    config.report_title = "Automação web - Tangerino"
+    config.report_title = "Automação web - Noesis"
     config.compress_image = true
     config.additional_info = {
       "Aplicação: " => "Web",
